add2fb7d3f1a09deeadcb6698826d36b35bf0b29
Cheri <mightypillow@tutanota.com>
2021-01-22 13:09:59 -0500

    Write function that displays each book on page

--- library.js
+++ library.js
@@ -1,3 +1,4 @@
+const booksDisplay = document.querySelector("main");
 const myLibrary = [];
 
 function Book(title, author, pages, hasRead) {
@@ -11,10 +12,41 @@ function addBookToLibrary(title, author, pages, hasRead = false) {
   myLibrary.push(new Book(title, author, pages, hasRead));
 }
 
+function displayBooks() {
+  myLibrary.forEach(book => {
+    const bookCard = document.createElement("article");
+    const heading = document.createElement("header");
+    const titleDisplay = document.createElement("h1");
+    const info = document.createElement("div");
+    const authorDisplay = document.createElement("h2");
+    const pagesDisplay = document.createElement("p");
+    const readDisplay = document.createElement("p");
+
+    titleDisplay.textContent = book.title;
+    authorDisplay.textContent = book.author;
+    pagesDisplay.textContent = `${book.pages} pages`;
+    readDisplay.textContent = book.hasRead ? "Read" : "Want to Read";
+
+    heading.appendChild(titleDisplay);
+    info.appendChild(authorDisplay);
+    info.appendChild(pagesDisplay);
+    info.appendChild(readDisplay);
+
+    bookCard.classList.add("book");
+    bookCard.appendChild(heading);
+    bookCard.appendChild(info);
+
+    booksDisplay.appendChild(bookCard);
+  });
+}
+
 addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 324, true);
 addBookToLibrary("Pride and Prejudice", "Jane Austen", 279, false);
 addBookToLibrary("1984", "George Orwell", 237, false);
 addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 200, true);
+addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1216, false);
+addBookToLibrary("Charlotte's Web", "E.B. White", 184, true);
+addBookToLibrary("Of Mice and Men", "John Steinbeck", 103, true);
+addBookToLibrary("Fehrenheit 451", "Ray Bradbury", 194, false);
 
-console.table(myLibrary);
-console.log(myLibrary[0]);
+displayBooks();
