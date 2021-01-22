48f4a4da53d778c11fc2c6b608a58cc38e827947
Cheri <mightypillow@tutanota.com>
2021-01-22 15:19:01 -0500

    New book form submits and displays

--- library.js
+++ library.js
@@ -1,4 +1,11 @@
 const booksDisplay = document.querySelector("main");
+const newBookBtn = document.querySelector("button");
+const newBookFormDisplay = document.querySelector("body > div");
+const newBookForm = document.querySelector("form");
+const newBookTitleInput = document.querySelector("#title");
+const newBookAuthorInput = document.querySelector("#author");
+const newBookPagesInput = document.querySelector("#pages")
+const newBookHasReadInput = document.querySelector("#has-read");
 const myLibrary = [];
 
 function Book(title, author, pages, hasRead) {
@@ -8,11 +15,18 @@ function Book(title, author, pages, hasRead) {
   this.hasRead = hasRead;
 }
 
-function addBookToLibrary(title, author, pages, hasRead = false) {
+function addBookToLibrary(title, author, pages, hasRead) {
   myLibrary.push(new Book(title, author, pages, hasRead));
 }
 
+function refreshBooksDisplay() {
+  while(booksDisplay.hasChildNodes()) {
+    booksDisplay.removeChild(booksDisplay.lastChild);
+  }
+}
+
 function displayBooks() {
+  refreshBooksDisplay();
   myLibrary.forEach(book => {
     const bookCard = document.createElement("article");
     const heading = document.createElement("header");
@@ -40,6 +54,22 @@ function displayBooks() {
   });
 }
 
+newBookBtn.addEventListener("click", () => {
+  newBookFormDisplay.style.display = "block";
+});
+
+newBookForm.addEventListener("submit", (e) => {
+  e.preventDefault(); // submitting form doesn't refresh the page
+  addBookToLibrary(
+      newBookTitleInput.value, 
+      newBookAuthorInput.value, 
+      newBookPagesInput.value, 
+      newBookHasReadInput.checked
+  );
+  displayBooks();
+  newBookFormDisplay.style.display = "none";
+});
+
 addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 324, true);
 addBookToLibrary("Pride and Prejudice", "Jane Austen", 279, false);
 addBookToLibrary("1984", "George Orwell", 237, false);
@@ -48,5 +78,7 @@ addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1216, false);
 addBookToLibrary("Charlotte's Web", "E.B. White", 184, true);
 addBookToLibrary("Of Mice and Men", "John Steinbeck", 103, true);
 addBookToLibrary("Fehrenheit 451", "Ray Bradbury", 194, false);
+addBookToLibrary("The Outsiders", "S.E. Hinton", 192, true);
+addBookToLibrary("A Tale of Two Cities", "Charles Dickens", 489, false);
 
 displayBooks();
